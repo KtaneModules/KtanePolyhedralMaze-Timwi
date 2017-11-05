ebede00f53936196bdbf5500fc1330bfbcc371c2
Timwi <github@timwi.de>
2017-11-05 08:12:41 +0100

    First commit

---
+++ Assets/PentagonalHexecontahedralMazeModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using PentagonalHexecontahedralMaze;
+using UnityEngine;
+using Rnd = UnityEngine.Random;
+
+/// <summary>
+/// On the Subject of Pentagonal-Hexecontahedral Maze
+/// Created by Timwi
+/// </summary>
+public class PentagonalHexecontahedralMazeModule : MonoBehaviour
+{
+    public KMBombInfo Bomb;
+    public KMBombModule Module;
+    public KMAudio Audio;
+
+    private static int _moduleIdCounter = 1;
+    private int _moduleId;
+
+    void Start()
+    {
+        _moduleId = _moduleIdCounter++;
+    }
+}
