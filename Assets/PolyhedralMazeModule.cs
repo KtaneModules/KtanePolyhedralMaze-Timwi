9aab382baea54c56f97105337b9ab8c06c49d8db
Timwi <github@timwi.de>
2018-04-30 17:35:13 +0200

    Some polyhedra don’t have paths 7 or more steps long

--- Assets/PolyhedralMazeModule.cs
+++ Assets/PolyhedralMazeModule.cs
@@ -38,7 +38,7 @@ public class PolyhedralMazeModule : MonoBehaviour
     private int[] _clockfaceToArrow = new int[12];
     private bool _coroutineActive = false;
 
-    private const int _minSteps = 7;
+    private const int _minSteps = 5;
     private const int _maxSteps = 11;
 
     void Start()
